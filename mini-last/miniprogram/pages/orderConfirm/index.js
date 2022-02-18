10b3c63c3f92e8c52229b8630c8d2ac97add3081
ZeroIce <zeroicegc@163.com>
2022-02-18 12:14:05 +0800

    订单确认-页面

--- mini-last/miniprogram/pages/orderConfirm/index.js
+++ mini-last/miniprogram/pages/orderConfirm/index.js
@@ -5,62 +5,81 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    current: 'tab1',
+    max: 200, //限制最大输入字符数
+    min: 1,  //限制最小输入字符数
+    minWord: '',//提示语句
+    height: 0,
+    latitude: 39.89631551,
+    longitude: 116.323459711,
+    markers: [{
+      id: 1,
+      latitude: 39.89631551,
+      longitude: 116.323459711,
+      name: '虎虎春联购'
+    }],
+    covers: [{
+      latitude: 39.89631551,
+      longitude: 116.323459711,
+      iconPath: '/image/location.png'
+    }, {
+      latitude: 39.89631551,
+      longitude: 116.323459711,
+      iconPath: '/image/location.png'
+    }],
+    address: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let _this = this;
+    wx.getSystemInfo({
+      success: (result) => {
+        _this.setData({
+          height: result.windowHeight - 140
+        })
+      },
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleChange({ detail }) {
+    this.setData({
+      current: detail.key
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onReady: function (e) {
+    this.mapCtx = wx.createMapContext('myMap')
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  navigation: function () {
+    let plugin = requirePlugin('routePlan');
+    let key = 'CWXBZ-JSM6U-KCJV5-2MKJ7-R6PO3-GZBA3';  //使用在腾讯位置服务申请的key
+    let referer = '虎虎春联购';   //调用插件的app的名称
+    let endPoint = JSON.stringify({  //终点
+      'name': '吉野家(北京西站北口店)',
+      'latitude': 39.89631551,
+      'longitude': 116.323459711
+    });
+    wx.navigateTo({
+      url: 'plugin://routePlan/index?key=' + key + '&referer=' + referer + '&endPoint=' + endPoint
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getValueLength: function (e) {
+    let value = e.detail.value
+    let len = parseInt(value.length)
+    //最少字数限制
+    if (len <= this.data.min)
+      this.setData({
+        minWord: "至少填写10个字哦～"
+      })
+    else if (len > this.data.min)
+      this.setData({
+        minWord: " "
+      })
+    //最多字数限制
+    if (len > 200) return;
+    this.setData({
+      currentWordNumber: len //当前字数 
+    })
   }
 })
--- mini-last/miniprogram/pages/shopping/index.js
+++ mini-last/miniprogram/pages/shopping/index.js
@@ -8,6 +8,7 @@ Page({
     height: 0,
     lists: ["盒联1.1m", "盒联1.3m", "盒联1.6m", "盒联2.2m", "盒联3.0m",
       "二开直联", "二开半直联", "三开直联", "四开直联", "五开直联", "红包", "中国结"],
+    show: false
   },
 
   /**
@@ -21,6 +22,16 @@ Page({
           height: result.windowHeight - 50
         })
       },
+    })
+  },
+  close: function () {
+    this.setData({
+      show: !this.data.show
+    })
+  },
+  selectDone: function () {
+    wx.navigateTo({
+      url: '../orderConfirm/index',
     })
   }
 })
