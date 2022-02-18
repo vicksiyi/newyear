70a6a084fc8d659518c6331fc11d3fe89320684a
ZeroIce <zeroicegc@163.com>
2022-02-18 15:48:00 +0800

    物流& 订单详情-页面

--- mini-last/miniprogram/pages/order/index.js
+++ mini-last/miniprogram/pages/order/index.js
@@ -5,7 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    orderDetail: 'WD200012378136',
+    orderNum: 'WD200012378136',
+    orderTime: '2022-01-03 16：51：25',
   },
 
   /**
@@ -14,53 +16,9 @@ Page({
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  showDetail: function () {
+    wx.navigateTo({
+      url: 'plugin://kdPlugin/index?num=SF1141900154711&appName=虎虎春联购',
+    })
   }
 })
